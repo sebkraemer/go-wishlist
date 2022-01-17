fe3a86d78c06579c98ba66eaf503e567d887d24f
Sebastian Krämer <sebkraemer@posteo.org>
2022-01-17 23:55:27 +0100

    day 67: first mongodb program, listing the databases

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Item struct {
+	Owner string    `json:owner`
+	Desc  string    `json:desc`
+	Date  time.Time `json:date`
+}
+
+func main() {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://172.16.44.133:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("available databases: %s\n", databases)
+}
