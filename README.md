d03159493b43307e51e00eb0972f5cf808c8f9c4
Sebastian Krämer <sebkraemer@posteo.org>
2022-01-18 00:40:03 +0100

    day 68: adding the first wish, updated README

--- README.md
+++ README.md
@@ -9,8 +9,33 @@
 Start mongodb service:
 `docker-compose -f ./docker/docker-compose.yml up`
 
+# Ideas, TODOs
+
+-[] make db connection configurable
+-[] implement CRUD functionality
+-[] expose REST interface
+-[] middleware
+  -[] logger
+  -[] auth
+  -[] web interface
+-[] authentication
+  -[] mongodb password protection
+  -[] authentication by application
+  -[] user accounts, create and manage
+  -[] authenticate users in web interface, JWT?
+-[] deployment, where and how
+-[] insert layer to support other nosql dbs, e.g. dynamodb
+  (see also link section for aws example)
+
 # Links and resources
 
 - mongo and setting it up in docker
   - https://www.mongodb.com/compatibility/docker
   - https://docs.docker.com/compose/compose-file/compose-file-v3
+  - MongoDB university, free courses and possibly vouchers for certification
+    - https://university.mongodb.com/courses/catalog
+    - kudos @MohsenKamranii for the hint. https://twitter.com/mohsenkamranii/status/1482500357389701122?s=21
+  - https://docs.mongodb.com/drivers/go/current/
+  - https://docs.mongodb.com/manual/crud/
+- https://dev.to/joojodontoh/build-user-authentication-in-golang-with-jwt-and-mongodb-2igd
+- https://pkg.go.dev/go.mongodb.org/mongo-driver@v1.8.0/mongo#example-Connect-AWS
--- main.go
+++ main.go
@@ -6,33 +6,36 @@ import (
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Item struct {
-	Owner string    `json:owner`
-	Desc  string    `json:desc`
-	Date  time.Time `json:date`
+type WishlistItem struct {
+	Owner string    `json:"owner" bson:"owner"`
+	Desc  string    `json:"desc"  bson:"desc"`
+	Date  time.Time `json:"date"  bson:"date"`
 }
 
 func main() {
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://172.16.44.133:27017"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://172.16.44.133:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
+
+	collection := client.Database("wishlist").Collection("wishes")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
 
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	testItem := WishlistItem{"seb", "test wish", time.Now()}
+
+	res, err := collection.InsertOne(context.Background(), testItem)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("available databases: %s\n", databases)
+	fmt.Printf("Inserted %v with id %v\n", testItem, res.InsertedID)
 }
